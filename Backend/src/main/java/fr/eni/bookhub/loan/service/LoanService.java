ce817950bfd58f46f4df7b646ef9111537d2f6d2
MattAzzip <matthieu.robert2026@campus-eni.fr>
2026-07-27 12:27:29 +0200

    #12 :
    + Rédaction des commentaires pour la Documentation Java du Service.
    + Création des Méthodes contenant la logique métier.

--- Backend/src/main/java/fr/eni/bookhub/loan/service/LoanService.java
+++ Backend/src/main/java/fr/eni/bookhub/loan/service/LoanService.java
@@ -1,12 +1,16 @@
 package fr.eni.bookhub.loan.service;
 
-import fr.eni.bookhub.exception.custom.LoanException;
+import fr.eni.bookhub.exception.custom.ConflictException;
 import fr.eni.bookhub.loan.dao.ILoanDao;
+import fr.eni.bookhub.loan.dto.response.LoanDTO;
 import fr.eni.bookhub.loan.entity.Loan;
 import fr.eni.bookhub.loan.entity.LoanStatus;
+import fr.eni.bookhub.security.AuthenticatedUserProvider;
+import fr.eni.bookhub.user.entity.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -14,34 +18,114 @@ import java.util.List;
 public class LoanService {
 
     private final ILoanDao loanRepository;
+    private final AuthenticatedUserProvider authenticatedUserProvider;
 
-    public List<Loan> findAll() {
-        return loanRepository.findAll();
+    /*
+    Method in charge to find all loans on database.
+     */
+    public List<LoanDTO> findAll() {
+        return loanRepository.findAll()
+                .stream()
+                .map(LoanDTO::new)
+                .toList();
     }
 
-    public Loan findLoanById(Long id) {
+    /*
+    Method in charge to find one loan with its id and return a LoanDTO.
+    @id : id of the loan you want to find.
+     */
+    public LoanDTO findLoanById(Long id) {
 
-        if (loanRepository.findById(id) != null) {
-            return loanRepository.findById(id);
-        } else {
-            throw new LoanException("Loan not found");
+        if (loanRepository.findById(id) == null) {
+            throw new ConflictException("Loan not found");
         }
+
+        return new LoanDTO(loanRepository.findById(id));
 
     }
 
-    public void returnLoan(Long id) {
-        Loan loanfound = this.findLoanById(id);
+    public List<LoanDTO> findLoansByAuthenticatedUser() {
+        return loanRepository.findByLoanerId(authenticatedUserProvider.getCurrentUser().getId())
+                .stream()
+                .map(LoanDTO::new)
+                .toList();
+    }
 
+    /*
+    Method in charge to find one loan with its id.
+    @id : id of the loan you want to find.
+     */
+    public Loan getLoanEntityById(Long id) {
 
-        if (loanfound != null) {
-            if (loanfound.getStatus().equals(LoanStatus.RETURN) || loanfound.getReturnDate() != null) {
-                throw new LoanException("Loan already returned");
-            }
-
-
-        } else {
-            throw new LoanException("Loan not found");
+        if (loanRepository.findById(id) == null) {
+            throw new ConflictException("Loan not found");
         }
 
+        return loanRepository.findById(id);
+
+    }
+
+    /*
+    Method in charge to make a new loan if the book searched isn't loan.
+    @id : id of the loan you want to find.
+     */
+    public void createLoan(Long bookCopyId) {
+        User currentUser = authenticatedUserProvider.getCurrentUser();
+
+        if (bookCopyService.canBeLoaned(bookCopyId)) {
+            throw new ConflictException("Book already loaned");
+        }
+
+        Loan newLoan = Loan.builder()
+                .loaner(currentUser)
+                .bookCopyLoaned(bookCopyService.findById(bookCopyId))
+                .loanDate(LocalDate.now())
+                .status(LoanStatus.ACTIVE)
+                .build();
+
+        loanRepository.save(newLoan);
+    }
+
+    /*
+    Method in charge to declare to return a loan when the book return to the library.
+    @id : id of the loan you want to close.
+     */
+    public LoanDTO returnLoan(Long id) {
+        Loan loanFound = this.getLoanEntityById(id);
+
+        if (loanFound == null) {
+            throw new ConflictException("Loan not found");
+        }
+
+        if (loanFound.getStatus().equals(LoanStatus.RETURN) || loanFound.getReturnDate() != null) {
+            throw new ConflictException("Loan already returned");
+        }
+
+        loanFound.setReturnDate(LocalDate.now());
+
+        if (this.IsLoanReturnDelayed(loanFound) && loanFound.getDueDate() == null) {
+            loanFound.setDueDate(LocalDate.now());
+            return new LoanDTO(loanFound);
+        } else {
+            loanFound.setStatus(LoanStatus.RETURN);
+            loanRepository.save(loanFound);
+            return null;
+        }
+
+
+    }
+
+    /*
+    Method in charge to determinate if a loan is returned in time.
+    @loan Object Loan you want to test.
+     */
+    public boolean IsLoanReturnDelayed(Loan loan) {
+        LocalDate dateEmprunt = loan.getLoanDate();
+        LocalDate dateReturn = dateEmprunt.plusDays(14);
+
+        if (loan.getReturnDate().isAfter(dateReturn)) {
+            return true;
+        }
+        return false;
     }
 }
