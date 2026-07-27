c54f80e8678da84ca5db8e74e3ad6062126ceab0
MattAzzip <matthieu.robert2026@campus-eni.fr>
2026-07-27 12:22:28 +0200

    #12 :
    + Ajout des ResponseEntity lors des réponses de l'API
    
    + Ajout des méthodes des différentes routes.

--- Backend/src/main/java/fr/eni/bookhub/loan/controller/LoanController.java
+++ Backend/src/main/java/fr/eni/bookhub/loan/controller/LoanController.java
@@ -1,28 +1,38 @@
 package fr.eni.bookhub.loan.controller;
 
-import fr.eni.bookhub.loan.entity.Loan;
+import fr.eni.bookhub.loan.dto.response.LoanDTO;
 import fr.eni.bookhub.loan.service.LoanService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/loan")
+@RequestMapping("/api/loans")
 @AllArgsConstructor
 public class LoanController {
 
     private final LoanService loanService;
 
     @GetMapping("")
-    public List<Loan> findAll() {
-        return loanService.findAll();
+    public ResponseEntity<List<LoanDTO>> findAll() {
+        return ResponseEntity.ok().body(loanService.findAll());
     }
 
     @GetMapping("/my")
+    public ResponseEntity<List<LoanDTO>> findAllMy() {
+        return ResponseEntity.ok().body(loanService.findLoansByAuthenticatedUser());
+    }
 
     @PutMapping("/{id}/return") // En tant que LIBRARIAN
-    public void returnLoans(@PathVariable Long id) {
-        loanService.returnLoan(id);
+    public ResponseEntity<LoanDTO> returnLoans(@PathVariable Long id) {
+
+        LoanDTO returnLoan = loanService.returnLoan(id);
+
+        if (returnLoan != null) {
+            return ResponseEntity.ok().body(returnLoan);
+        }
+        return ResponseEntity.ok().build();
     }
 }
