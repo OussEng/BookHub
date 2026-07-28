65a0127eaed21a7d6245d34dee20f4308f2b22ca
MattAzzip <matthieu.robert2026@campus-eni.fr>
2026-07-28 09:54:28 +0200

    feat: Replace ConflictException with LoanException for improved error handling in LoanService

--- Backend/src/main/java/fr/eni/bookhub/loan/service/LoanService.java
+++ Backend/src/main/java/fr/eni/bookhub/loan/service/LoanService.java
@@ -3,7 +3,7 @@ package fr.eni.bookhub.loan.service;
 import fr.eni.bookhub.bookcopy.entity.BookCopy;
 import fr.eni.bookhub.bookcopy.repository.BookCopyRepository;
 import fr.eni.bookhub.bookcopy.service.BookCopyService;
-import fr.eni.bookhub.exception.custom.ConflictException;
+import fr.eni.bookhub.exception.custom.LoanException;
 import fr.eni.bookhub.loan.dao.ILoanDao;
 import fr.eni.bookhub.loan.dto.response.LoanDTO;
 import fr.eni.bookhub.loan.entity.Loan;
@@ -25,6 +25,9 @@ public class LoanService {
     private final BookCopyService bookCopyService;
     private final BookCopyRepository bookCopyRepository;
 
+
+// --- CRUD ---
+
     /*
     Method in charge to find all loans on database.
      */
@@ -42,13 +45,17 @@ public class LoanService {
     public LoanDTO findLoanById(Long id) {
 
         if (loanRepository.findById(id) == null) {
-            throw new ConflictException("Loan not found");
+            throw new LoanException("Loan not found");
         }
 
         return new LoanDTO(loanRepository.findById(id));
 
     }
 
+
+    /*
+    Method in charge to find loans of the authenticated user.
+    */
     public List<LoanDTO> findLoansByAuthenticatedUser() {
         return loanRepository.findByLoanerId(authenticatedUserProvider.getCurrentUser().getId())
                 .stream()
@@ -63,7 +70,7 @@ public class LoanService {
     public Loan getLoanEntityById(Long id) {
 
         if (loanRepository.findById(id) == null) {
-            throw new ConflictException("Loan not found");
+            throw new LoanException("Loan not found");
         }
 
         return loanRepository.findById(id);
@@ -72,14 +79,18 @@ public class LoanService {
 
     /*
     Method in charge to make a new loan if the book searched isn't loan.
-    @id : id of the loan you want to find.
+    @id : id of the book copy you want to loan.
      */
     public void createLoan(Long id) {
         User currentUser = authenticatedUserProvider.getCurrentUser();
-        BookCopy bookCopy = bookCopyRepository.findById(id).orElseThrow(() -> new ConflictException("Book Copy not found"));
+        BookCopy bookCopy = bookCopyRepository.findById(id).orElseThrow(() -> new LoanException("Book Copy not found"));
 
-        if (bookCopyService.canBeLoaned(bookCopy)) {
-            throw new ConflictException("Book already loaned");
+        if (!bookCopyService.canBeLoaned(bookCopy)) {
+            throw new LoanException("Book already loaned");
+        }
+
+        if (loanRepository.countByLoanerIdAndStatus(currentUser.getId(), LoanStatus.ACTIVE) >= 5) {
+            throw new LoanException("Maximum books limit reached");
         }
 
         Loan newLoan = Loan.builder()
@@ -96,30 +107,28 @@ public class LoanService {
     Method in charge to declare to return a loan when the book return to the library.
     @id : id of the loan you want to close.
      */
-    public LoanDTO returnLoan(Long id) {
+    public void returnLoan(Long id) {
         Loan loanFound = this.getLoanEntityById(id);
 
         if (loanFound == null) {
-            throw new ConflictException("Loan not found");
+            throw new LoanException("Loan not found");
         }
 
         if (loanFound.getStatus().equals(LoanStatus.RETURN) || loanFound.getReturnDate() != null) {
-            throw new ConflictException("Loan already returned");
+            throw new LoanException("Loan already returned");
+        }
+
+        if (this.IsLoanReturnDelayed(loanFound)) {
+            loanFound.setDueDate(LocalDate.now());
         }
 
         loanFound.setReturnDate(LocalDate.now());
-
-        if (this.IsLoanReturnDelayed(loanFound) && loanFound.getDueDate() == null) {
-            loanFound.setDueDate(LocalDate.now());
-            return new LoanDTO(loanFound);
-        } else {
-            loanFound.setStatus(LoanStatus.RETURN);
-            loanRepository.save(loanFound);
-            return null;
-        }
-
+        loanFound.setStatus(LoanStatus.RETURN);
+        loanRepository.save(loanFound);
 
     }
+
+// --- Utils ---
 
     /*
     Method in charge to determinate if a loan is returned in time.
