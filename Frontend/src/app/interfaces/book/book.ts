a8bbff51f2a01990051e2e398b0aac40e580dc29
MattAzzip <matthieu.robert2026@campus-eni.fr>
2026-07-28 16:31:48 +0200

    feat: Enhance book details with loan functionality and add LoansResponseModel

--- Frontend/src/app/interfaces/book/book.ts
+++ Frontend/src/app/interfaces/book/book.ts
@@ -1,9 +1,9 @@
 export interface Book {
 
-    id: number;
-    title: string;
-    img: string;
-    author: string[];
-    available: boolean;
+  id: number;
+  title: string;
+  img: string;
+  author: string[];
+  available: boolean;
 
 }
