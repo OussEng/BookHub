346b53f02bae6ab954c72a698c1fa53180158e7d
mtieliehin2026 <mykhailo.tieliehin2026@campus-eni.fr>
2026-07-30 12:08:53 +0200

    remove import in BookDaoImpl

--- Backend/src/main/java/fr/eni/bookhub/book/dao/BookDaoImpl.java
+++ Backend/src/main/java/fr/eni/bookhub/book/dao/BookDaoImpl.java
@@ -1,6 +1,5 @@
 package fr.eni.bookhub.book.dao;
 
-import aj.org.objectweb.asm.commons.Remapper;
 import fr.eni.bookhub.book.entity.Book;
 import fr.eni.bookhub.book.repository.BookRepository;
 import lombok.AllArgsConstructor;
