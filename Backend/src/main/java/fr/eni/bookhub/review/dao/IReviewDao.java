9d2511ad5d33fc28d97a4fbd0ac488c3b804baec
Mehdi Rochereau <271727509+mehdi-rochereau@users.noreply.github.com>
2026-07-30 21:57:47 +0200

    fix: remove unused import in IReviewDao.java

--- Backend/src/main/java/fr/eni/bookhub/review/dao/IReviewDao.java
+++ Backend/src/main/java/fr/eni/bookhub/review/dao/IReviewDao.java
@@ -3,7 +3,6 @@ package fr.eni.bookhub.review.dao;
 import fr.eni.bookhub.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
