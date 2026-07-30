32c573c3b02f12d43e4a6e038842daf37301cbd8
OussEnd <oaitlahssaine@gmail.com>
2026-07-30 14:27:42 +0200

    feat: Enhance librarian dashboard layout with quick actions and improved book display

--- Frontend/src/app/pages/backoffice/librarian/librarian-home/librarian-home.ts
+++ Frontend/src/app/pages/backoffice/librarian/librarian-home/librarian-home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Book } from '../../../../interfaces/book/Book';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-librarian-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './librarian-home.html',
   styleUrl: './librarian-home.css',
 })
